d99085d277ebafa2711960c20658aefc3d1509ff
Pysuper <1821764535@qq.com>
2019-05-13 14:32:24 +0800

    一级标题

--- info/info/spiders/jd.py
+++ info/info/spiders/jd.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from info.items import InfoItem
 
 
 class JdSpider(scrapy.Spider):
@@ -8,8 +9,29 @@ class JdSpider(scrapy.Spider):
     start_urls = ['http://www.jd.com/']
 
     def parse(self, response):
-        cate_1_href = response.xpath('//li[@class="cate_menu_item"]/a/@href').extract()
-        cate_1_title = response.xpath('//li[@class="cate_menu_item"]/a/text()').extract()
+        # 获取京东下面的电脑分类数据
+        """
+        extract(): 返回的是一个数组list, 里面包含了多个string, 如果只有一个string, 则返回['ABC']这样的形式
+        extract_first(): 返回的是一个string字符串, 是list数组里面的第一个字符串
+        """
+        computer = response.xpath('//li[@clstag="h|keycount|head|category_03a"]/a[1]/@href').extract()
+        for cate_href in computer:
+            item = InfoItem()
+            computer_href = "https:" + cate_href
+            print(computer_href)
+            yield scrapy.Request(
+                url=computer_href,
+                callback=self.parse_detail,
+                meta={
+                    "item":item
+                }
+            )
 
-        for href_1, title_1 in zip(cate_1_href, cate_1_title):
-            print(href_1, title_1)
+    def parse_detail(self, response):
+        print(response.url)
+        cate_2_title = response.xpath('//div[@class="menu-cont"]/a/text()').extract()
+        cate_2_href = response.xpath('//div[@class="menu-cont"]/@href').extract()
+        print(cate_2_href, cate_2_title)
+
+        # for title, href in zip(cate_2_title, cate_2_href):
+        #     print(title, href)
